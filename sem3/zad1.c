b0f4d7a3ad0af9a4c7cc3bfb405f26a51339e63e
maciekzelazo <maciej.zelazo@onet.pl>
2020-10-22 16:53:04 +0200

    Update zad1.c

--- sem3/zad1.c
+++ sem3/zad1.c
@@ -1,2 +1,12 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
+
+int main(int argc, char *argv[])
+{
+    float b;
+    float a;
+    a = atof(argv[1]);
+    b = a*a;
+    printf("Wartość %f do kwadratu to %f.\n", a, b);
+}
